b8396b262ac0654158c8f3e0274228f95ea38bc5
Konstantin Rudenko <k.rudenko@foodpanda.com>
2016-01-06 11:36:14 +0100

    LOGI-82 add semi bold for Montserrat

--- app/src/main/java/com/foodpanda/urbanninja/ui/widget/MontserratButton.java
+++ app/src/main/java/com/foodpanda/urbanninja/ui/widget/MontserratButton.java
@@ -27,7 +27,14 @@ public class MontserratButton extends Button {
     }
 
     private void applyCustomFont(Context context) {
-        Typeface customFont = FontCacheUtil.getTypeface("Montserrat-Bold.ttf", context);
-        setTypeface(customFont);
+        Typeface currentTypeFace = getTypeface();
+        Typeface customFont;
+        if (currentTypeFace != null && currentTypeFace.getStyle() == Typeface.BOLD) {
+            customFont = FontCacheUtil.getTypeface("Montserrat-Bold.ttf", context);
+            setTypeface(customFont, Typeface.BOLD);
+        } else {
+            customFont = FontCacheUtil.getTypeface("Montserrat-Regular.ttf", context);
+            setTypeface(customFont);
+        }
     }
 }
