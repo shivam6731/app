c4fc753b6a96bbf0fcf6b9691c694867cc460973
Konstantin Rudenko <k.rudenko@foodpanda.com>
2016-03-11 15:33:24 +0100

    LOGI-151 add font

--- app/src/main/java/com/foodpanda/urbanninja/api/ApiUrl.java
+++ app/src/main/java/com/foodpanda/urbanninja/api/ApiUrl.java
@@ -2,7 +2,7 @@ package com.foodpanda.urbanninja.api;
 
 public class ApiUrl {
     private static final String SCHEME = "http://";
-    private static final String URL = "cis-fleet-dev-2.elasticbeanstalk.com/";
+    private static final String URL = "cis-fleet-dev-1.eu-west-1.elasticbeanstalk.com/";
 
     public static final String BASE_URL = SCHEME + URL;
 
